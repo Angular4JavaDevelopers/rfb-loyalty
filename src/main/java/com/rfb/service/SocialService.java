688b0e249905e6a4cf4b49b2d4803fe738c65526
Dan Vega <danvega@gmail.com>
2017-10-14 16:07:10 -0400

    adding facebook login code minus the secret info

--- src/main/java/com/rfb/service/SocialService.java
+++ src/main/java/com/rfb/service/SocialService.java
@@ -16,6 +16,9 @@ import org.springframework.social.connect.UserProfile;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
@@ -35,6 +38,27 @@ public class SocialService {
     private final UserRepository userRepository;
 
     private final MailService mailService;
+
+    @PostConstruct
+    private void init() {
+        try {
+            String[] fieldsToMap = {
+                "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type","is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format","political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other","sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "viewer_can_send_gift","website", "work"
+            };
+
+            Field field = Class.forName("org.springframework.social.facebook.api.UserOperations").
+                getDeclaredField("PROFILE_FIELDS");
+            field.setAccessible(true);
+
+            Field modifiers = field.getClass().getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, fieldsToMap);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public SocialService(UsersConnectionRepository usersConnectionRepository, AuthorityRepository authorityRepository,
             PasswordEncoder passwordEncoder, UserRepository userRepository,
