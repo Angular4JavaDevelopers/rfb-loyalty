a12916b5fe84d0d30a6752f6984ea04b7cc64e89
Dan Vega <danvega@gmail.com>
2017-10-21 18:14:11 -0400

    updating look & feel of the home page

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -8,6 +8,6 @@ export const HOME_ROUTE: Route = {
     component: HomeComponent,
     data: {
         authorities: [],
-        pageTitle: 'Welcome, Java Hipster!'
+        pageTitle: 'Running for Brews Loyalty App'
     }
 };
