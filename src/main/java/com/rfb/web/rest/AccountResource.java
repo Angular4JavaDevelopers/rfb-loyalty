19bacf469265f9c24043927feb2f727935118143
Dan Vega <danvega@gmail.com>
2017-11-04 14:08:55 -0400

    allow a user to set their home location from their account settings

--- src/main/java/com/rfb/web/rest/AccountResource.java
+++ src/main/java/com/rfb/web/rest/AccountResource.java
@@ -159,7 +159,7 @@ public class AccountResource {
             .findOneByLogin(userLogin)
             .map(u -> {
                 userService.updateUser(userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(),
-                    userDTO.getLangKey(), userDTO.getImageUrl());
+                    userDTO.getLangKey(), userDTO.getImageUrl(),userDTO.getHomeLocation());
                 return new ResponseEntity(HttpStatus.OK);
             })
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Principal, AccountService } from '../../shared';
+import { Principal, AccountService, ResponseWrapper } from '../../shared';
+import {RfbLocation} from '../../entities/rfb-location/rfb-location.model';
+import {RfbLocationService} from '../../entities/rfb-location/rfb-location.service';
 
 @Component({
     selector: 'jhi-settings',
@@ -11,10 +13,12 @@ export class SettingsComponent implements OnInit {
     success: string;
     settingsAccount: any;
     languages: any[];
+    locations: RfbLocation[];
 
     constructor(
         private account: AccountService,
-        private principal: Principal
+        private principal: Principal,
+        private locationService: RfbLocationService
     ) {
     }
 
@@ -22,6 +26,7 @@ export class SettingsComponent implements OnInit {
         this.principal.identity().then((account) => {
             this.settingsAccount = this.copyAccount(account);
         });
+        this.loadLocations();
     }
 
     save() {
@@ -45,7 +50,20 @@ export class SettingsComponent implements OnInit {
             langKey: account.langKey,
             lastName: account.lastName,
             login: account.login,
-            imageUrl: account.imageUrl
+            imageUrl: account.imageUrl,
+            homeLocation: account.homeLocation
         };
+    }
+
+    loadLocations() {
+        this.locationService.query({
+            page: 0,
+            size: 100,
+            sort: ['locationName', 'ASC']}).subscribe(
+            (res: ResponseWrapper) => {
+                this.locations = res.json;
+            },
+            (res: ResponseWrapper) => { console.log(res) }
+        );
     }
 }
